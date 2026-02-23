f472000884845e698ba6a655c92b51bfbaaae191
Ikhwan A. Latif <ikhwanal235@gmail.com>
2026-02-23 12:17:28 +0700

    feat: add log in TopBar

--- web/src/TopBar.jsx
+++ web/src/TopBar.jsx
@@ -2,6 +2,8 @@ import { Brain } from "lucide-react"
 import "./TopBar.css"
 import { Plus } from "lucide-react"
 import { useRouter } from "./router"
+import { TrendingUp } from "lucide-react"
+import { List } from "lucide-react"
 
 export default function TopBar() {
   const { navigate } = useRouter()
@@ -20,13 +22,22 @@ export default function TopBar() {
           <span>Personal</span>
         </div>
         <div className="nav-tool">
-          <button onClick={handleRedirectRoute("/")}>Dashboard</button>
+          <button className="nav-tag" onClick={handleRedirectRoute("/")}>
+            <TrendingUp />
+            <div>Dashboard</div>
+          </button>
           <button className="nav-tag" onClick={handleRedirectRoute("/new-log")}>
             <Plus />
             <div>Log New Decision</div>
           </button>
-          <button>Decision</button>
-          <button>Insight</button>
+          <button className="nav-tag" onClick={handleRedirectRoute("/")}>
+            <List />
+            <div>Decision</div>
+          </button>
+          <button className="nav-tag" onClick={handleRedirectRoute("/")}>
+            <Brain />
+            <div>Insight</div>
+          </button>
         </div>
       </nav >
     </>
