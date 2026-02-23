b27acbb303d352252ef89df23c534ff1d289de16
Ikhwan A. Latif <ikhwanal235@gmail.com>
2026-02-23 14:42:11 +0700

    feat: Add Logic Behaviour For Form Log Decision
    Add Logic to Store Value and Logic to able create new input each click
    for multiple value

--- web/src/features/log-a-decision/FormInputDecision.jsx
+++ web/src/features/log-a-decision/FormInputDecision.jsx
@@ -4,33 +4,99 @@ import { XIcon } from "lucide-react";
 import "../../styles/text.css"
 
 import "./FormInputDecision.css"
+import { useState } from "react";
+import { useRef } from "react";
 
-export default function FormInputDecision() {
-  const createAdditionalInput = () => {
-    return <div className="additional-input">
+export default function FormInputDecision({ value, onChange, onChangeFailuresMode }) {
+  const idRef = useRef(1)
+  const [failures, setFailures] = useState(value.failures)
+  const [confidenceLevel, setConfidence] = useState(value.confidence)
+
+  const handleAddFailureMode = () => {
+    const newFailures = {
+      id: idRef.current,
+      values: ''
+    }
+    const nextFailures = [...failures, newFailures]
+
+    setFailures(nextFailures)
+    onChangeFailuresMode(nextFailures)
+
+    idRef.current += 1
+  }
+
+  const handleRemoveFailureMode = (id) => {
+    return () => {
+      const failureMode = failures.find((x) => x.id == id)
+      if (failureMode) {
+        const failuresMode = failures.filter(x => x.id != id)
+        setFailures(failuresMode)
+        onChangeFailuresMode(failuresMode)
+      }
+    }
+  }
+
+  const handleFailureInputChange = (id) => {
+    return (ev) => {
+      const failureMode = failures.find(x => x.id == id)
+      if (!failureMode) return
+
+      const nextFailures = failures.map(x => x.id == id ? { ...x, values: ev.target.value } : x)
+
+      setFailures(nextFailures)
+      onChangeFailuresMode(nextFailures)
+    }
+  }
+
+
+  const createAdditionalInput = (id, value) => {
+    return <div className="additional-input" key={id}>
       <AlertCircleIcon className="form-alert-icon" />
-      <input type="text" placeholder="e.g Company Culture does not match" />
-      <button className="button-icon">
+      <input type="text" placeholder="e.g Company Culture does not match" value={value} onChange={handleFailureInputChange(id)} />
+      <button className="button-icon" onClick={handleRemoveFailureMode(id)}>
         <XIcon className="form-remove-icon" size={14} />
       </button>
     </div>
+  }
+
+  const renderAllFailuresMode = () => {
+    return failures.map((v) => createAdditionalInput(v.id, v.values))
+  }
+
+  const handleConfidenceInput = (ev) => {
+    setConfidence(ev.target.value)
+    onChange("confidence")(ev)
   }
 
   return <>
     <div className="form-card">
       <div className="form-input">
         <label>What decision are you making? *</label>
-        <input type="text" className="less-important" placeholder="e.g Accepting a Job offer at Company X" />
+        <input
+          type="text"
+          className="less-important"
+          placeholder="e.g Accepting a Job offer at Company X"
+          onChange={onChange("decision")}
+          value={value["decision"]}
+        />
       </div>
 
       <div className="form-input">
         <label>What do you expect to happen? *</label>
-        <textarea rows="10" cols="60" placeholder="Something" className="less-important"></textarea>
+        <textarea
+          rows="10"
+          cols="60"
+          placeholder="Your Expectation"
+          className="less-important"
+          onChange={onChange("expectation")}
+          value={value["expectation"]}
+        >
+        </textarea>
       </div>
 
       <div className="form-input">
-        <label>Confidence Level: 50%</label>
-        <input type="range" className="slider" />
+        <label>Confidence Level: {confidenceLevel}%</label>
+        <input type="range" className="slider" min={0} max={100} onChange={handleConfidenceInput} value={confidenceLevel} />
         <div className="range-tag">
           <span>No Confidence</span>
           <span>Certain</span>
@@ -40,15 +106,18 @@ export default function FormInputDecision() {
       <div className="form-input">
         <label>What could go wrong?</label>
         <span>List potential failure modes to calibrate your risk assessment</span>
-        {createAdditionalInput()}
-        {createAdditionalInput()}
-        {createAdditionalInput()}
+        {renderAllFailuresMode()}
 
-        <button className="failure-mode"> <Plus /> Add Failure Mode</button>
+        <button className="failure-mode" onClick={handleAddFailureMode}> <Plus /> Add Failure Mode</button>
       </div>
       <div className="form-input">
         <label>Additional Notes (Optional)</label>
-        <textarea rows={5} cols={60}></textarea>
+        <textarea
+          rows={5}
+          cols={60}
+          onChange={onChange("additionalNotes")}
+          value={value["additionalNotes"]}
+        ></textarea>
       </div>
     </div>
   </>
--- web/src/features/log-a-decision/index.jsx
+++ web/src/features/log-a-decision/index.jsx
@@ -1,9 +1,47 @@
+import { useState } from "react";
 import Header from "../../component/Header";
+import { useRouter } from "../../router";
 import FormInputDecision from "./FormInputDecision";
+import "./index.css"
 
 export default function LogADecision() {
+  const [form, setForm] = useState({
+    decision: "",
+    expectation: "",
+    confidence: 50,
+    failures: [],
+    additionalNotes: ""
+  })
+
+  const { navigate } = useRouter()
+
+  const handleCancel = () => {
+    navigate("/")
+  }
+
+  const onChangeFailuresMode = (failures) => {
+    setForm(prev => ({ ...prev, failures: failures }))
+  }
+
+  const onChange = (key) => {
+    return (ev) => {
+      setForm(prev => ({
+        ...prev,
+        [key]: ev.target.value
+      }))
+    }
+  }
+
+  const onSubmit = () => {
+    console.log(form)
+  }
+
   return <>
     <Header headerInfo="Log a Decision" otherInfo="Record your prediction before the outcome is know" />
-    <FormInputDecision />
+    <FormInputDecision value={form} onChange={onChange} onChangeFailuresMode={onChangeFailuresMode} />
+    <div className="form-button">
+      <button className="cancel-button" onClick={handleCancel}>Cancel</button>
+      <button className="accept-button" onClick={onSubmit}>Log Decision</button>
+    </div>
   </>
 }
